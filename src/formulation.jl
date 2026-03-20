b9f1b381d5503dac9edae554b49f8246e87abaf9
Kiernan Jennings <kiernan.xj@gmail.com>
2026-03-20 09:01:20 -0500

    move some files and starting more work for today

--- src/formulation.jl
+++ src/formulation.jl
@@ -34,6 +34,138 @@ function MatpowerData(data::Dict{String, Any})
 end
 
 
+function ac_uc(file_path::String, type="Rectangular")
+    if type == "Rectangular"
+        return build_single_period_ac_uc_rectangular(file_path)
+    else
+        return build_single_period_ac_uc_polar(file_path)
+    end
+end
+
+function mp_ac_uc(file_path::String, demand_curve::Vector{Float64}, type="Rectangular")
+    if type == "Rectangular"
+        return build_mp_ac_uc_rectangular(file_path, demand_curve)
+    else
+        return build_mp_ac_uc_polar(file_path, demand_curve)
+    end
+end
+
+function build_single_period_ac_uc_polar(file_path::String)    
+    data = _parse_file_data(file_path)
+    T = [1]
+    # Initialize the JuMP Model
+    model = Model(Gurobi.Optimizer)
+
+    # Add variables 
+    _add_acuc_var_polar!(model, data, T)
+
+    # Add objective
+    _add_mincost_obj!(model, data, T)
+
+    # Add constraints
+    _add_ref_limits_polar!(model, data, T)
+
+    _add_gen_limits!(model, data, T)
+
+    _add_polar_branchflow!(model, data, T)
+
+    _add_node_bal_polar!(model, data, T)
+
+    return model
+end
+
+function build_mp_ac_uc_polar(file_path::String, demand_curve::Vector{Float64})
+    """
+    demand_curve::Vector{Float64} - scaled demand levels for each T
+    akin to ExaModelsPower
+    """
+    data = _parse_file_data(file_path)
+    T = [i for (i,val) in enumerate(demand_curve)]
+
+    # Initialize the JuMP Model
+    model = Model(Gurobi.Optimizer)
+
+    # Add variables 
+    _add_acuc_var_polar!(model, data, T)
+
+    # Add objective
+    _add_mincost_obj!(model, data, T)
+
+    # Add constraints
+    _add_ref_limits_polar!(model, data, T)
+
+    _add_gen_limits!(model, data, T)
+
+    _add_polar_branchflow!(model, data, T)
+
+    _add_node_bal_polar!(model, data, demand_curve)
+
+    return model
+end
+
+function build_mp_ac_uc_rectangular(file_path::String, demand_curve::Vector{Float64})
+    """
+    demand_curve::Vector{Float64} - scaled demand levels for each T
+    akin to ExaModelsPower
+    """
+    data = _parse_file_data(file_path)
+    T = [i for (i, _) in enumerate(demand_curve)]
+
+    # Initialize the JuMP Model
+    model = Model(Gurobi.Optimizer)
+
+    # Add variables 
+    _add_acuc_var_rectangular!(model, data, T)
+
+    # Add objective
+    _add_mincost_obj!(model, data, T)
+
+    # Add constraints
+
+    _add_ref_limits_rectangular!(model, data, T)
+    _add_gen_limits!(model, data, T)
+
+    _add_rectangular_branchflow!(model, data, T)
+
+    _add_node_bal_rectangular!(model, data, demand_curve)
+
+    return model
+end
+
+function build_single_ac_uc_rectangular(file_path::String)
+    data = _parse_file_data(file_path)
+    T = [1]
+
+    # Initialize the JuMP Model
+    model = Model(Gurobi.Optimizer)
+
+    # Add variables 
+    _add_acuc_var_rectangular!(model, data, T)
+
+    # Add objective
+    _add_mincost_obj!(model, data, T)
+    # Add constraints
+    _add_ref_limits_rectangular!(model, data, T) 
+    _add_gen_limits!(model, data, T)
+    _add_rectangular_branchflow!(model, data, T)
+    _add_node_bal_rectangular!(model, data, T)
+
+    return model
+
+end
+
+function build_convex_ac_uc(file_path::String)
+    """
+    Using Constante-Flores and Li 2026 convex QCQP approximation of ACPF
+    The reformulation comes from: 
+    c_ii = vr^2 + vi^2 forall n (1l)
+    c_ij = vi_i vi_j + vr_i vr_j forall i,j in E (1m)
+    s_ij = vr_i vi_j - vr_j vi_i forall i,j in E (1n)
+    """
+    
+end
+
+
 function _parse_file_data(file_path::String)::MatpowerData
     # 1. Parse the data
     data = PowerModels.parse_file(file_path)
@@ -69,22 +201,45 @@ function _add_acuc_var_polar!(model::JuMP.Model, data::MatpowerData, T::Vector{I
     @variable(model, q_to[keys(branches), T])
 end
 
+function _get_edges(data::MatpowerData)
+    # edges = [(string(branch["f_bus"]), string(branch["t_bus"])) for (i, branch) in data.branches]
+    edges = [(branch["f_bus"], branch["t_bus"]) for (i, branch) in data.branches]
+    return edges
+end
+
 function _add_acuc_var_rectangular!(model::JuMP.Model, data::MatpowerData, T::Vector{Int64})
     """
-    Adds multi-period ACUC variables for rectangular coords
+    Rectangular power voltage W-matrix style formulation
     """
-    # multi period opf
-    # buses, gens, branches, _ = _unpack_matpowerdata(data)
     buses, gens, branches, loads, shunts = (data.buses, data.gens, data.branches, data.loads, data.shunts)
 
     # 3. Define Variables
     @variable(model, vr[i in keys(data.buses), T]) # real voltage
     @variable(model, vi[i in keys(data.buses), T]) # imaginary voltage
+    @variable(model, c_ii[i in keys(data.buses), T])
+
+    edges = _get_edges(data) # vector of tuples listing the connections
 
     for (i, bus) in data.buses
-        @constraint(model, [t in T], model[:vr][i,t]^2 + model[:vi][i,t]^2 <= bus["vmax"]^2)
-        @constraint(model, [t in T], model[:vr][i,t]^2 + model[:vi][i,t]^2 >= bus["vmin"]^2)
+        @constraint(model, [t in T], model[:c_ii][i,t] == model[:vr][i,t]^2 + model[:vi][i,t]^2)
+        @constraint(model, [t in T], bus["vmin"]^2 <= model[:c_ii][i,t] <= bus["vmax"]^2)
     end
+    
+    # for (i, j) in edges
+        # define only for edges
+    @variable(model, c_ij[e in edges,T])
+    @variable(model, s_ij[e in edges,T])
+
+    for (i,j) in edges
+        # define eq. constraints for cij, sij
+        e = (i,j)
+        i = string(i); j = string(j)
+        @constraint(model, [t in T], model[:c_ij][e,t] == model[:vr][i,t]*model[:vr][j,t] + model[:vi][i,t]*model[:vi][j,t]) # "4" not found
+        @constraint(model, [t in T], model[:s_ij][e,t] == model[:vr][i,t]*model[:vi][j,t] - model[:vr][j,t]*model[:vi][i,t])
+        # data.branches["#"]["f_bus"] = i
+        # data.branches["#"]["t_bus"] = j
+    end
+    
     
     @variable(model, u[keys(gens), T], Bin) # UNIT COMMITMENT: Binary status
     @variable(model, pg[keys(gens), T])     # Active power generation
@@ -97,6 +252,72 @@ function _add_acuc_var_rectangular!(model::JuMP.Model, data::MatpowerData, T::Ve
     @variable(model, q_to[keys(branches), T])
 end
 
+function _add_acuc_var_convex!(model::JuMP.Model, data::MatpowerData, T::Vector{Int64})
+    """
+    Adjusting the following:
+    c_ii = vr^2 + vi^2 forall n (1l)
+    c_ij = vi_i vi_j + vr_i vr_j forall i,j in E (1m)
+    s_ij = vr_i vi_j - vr_j vi_i forall i,j in E (1n)
+    """
+    buses, gens, branches, loads, shunts = (data.buses, data.gens, data.branches, data.loads, data.shunts)
+
+    # 3. Define Variables
+    @variable(model, vr[i in keys(data.buses), T]) # real voltage
+    @variable(model, vi[i in keys(data.buses), T]) # imaginary voltage
+    @variable(model, c_ii[i in keys(data.buses), T])
+
+    @variable(model, xi_c[i in keys(data.buses),T] >= 0) # slack
+
+    edges = _get_edges(data) # vector of tuples listing the connections
+
+    for (i, bus) in data.buses
+        @constraint(model, [t in T], model[:c_ii][i,t] == model[:vr][i,t]^2 + model[:vi][i,t]^2)
+        @constraint(model, [t in T], bus["vmin"]^2 <= model[:c_ii][i,t] <= bus["vmax"]^2) # 1i
+
+        @constraint(model, [t in T], model[:c_ii][i, t] >= model[:vr][i,t]^2 + model[:vi][i,t]^2) #4b
+        @constraint(model, [t in T], )
+    end
+    
+    # for (i, j) in edges
+        # define only for edges
+    @variable(model, c_ij[e in edges,T])
+    @variable(model, s_ij[e in edges,T])
+    @variable(model, xij_c[e in edges,T] >= 0) # slack
+    @variable(model, xij_s[e in edges,T] >= 0) # slack
+
+    for (i,j) in edges
+        # define eq. constraints for cij, sij
+        e = (i,j)
+        i = string(i); j = string(j)
+        # @constraint(model, [t in T], model[:c_ij][e,t] == model[:vr][i,t]*model[:vr][j,t] + model[:vi][i,t]*model[:vi][j,t]) # "4" not found
+        # @constraint(model, [t in T], model[:s_ij][e,t] == model[:vr][i,t]*model[:vi][j,t] - model[:vr][j,t]*model[:vi][i,t])
+        # @constraint(model, [t in T], )
+    end
+ 
+    @variable(model, u[keys(gens), T], Bin) # UNIT COMMITMENT: Binary status
+    @variable(model, pg[keys(gens), T])     # Active power generation
+    @variable(model, qg[keys(gens), T])     # Reactive power generation
+
+    # Branch flow variables (from and to ends)
+    @variable(model, p_fr[keys(branches), T])
+    @variable(model, q_fr[keys(branches), T])
+    @variable(model, p_to[keys(branches), T])
+    @variable(model, q_to[keys(branches), T])
+end
+
+
+function _add_convex_constraints!(model::JuMP.Model, data::MatpowerData, T::Vector{Int64}, node_voltages)
+    """
+    Adding the RHS approximation of the nodal voltages Vr_i and Vi_i
+    These are the novelty of the Constante-Flores and Li 2026 work
+
+    * node_voltages is a dictionary that contains the mapping of the real and imaginary voltages for each node
+    """
+
+
+
+
+end
 
 function _add_mincost_obj!(model::JuMP.Model, data::MatpowerData, T::Vector{Int64})
     """
@@ -189,6 +410,7 @@ function _add_polar_branchflow!(model::JuMP.Model, data::MatpowerData, T::Vector
 end
 
 
+
 function _add_rectangular_branchflow!(model::JuMP.Model, data::MatpowerData, T::Vector{Int64})
     for t in T, (i, branch) in data.branches
         f_bus = string(branch["f_bus"])
@@ -202,37 +424,38 @@ function _add_rectangular_branchflow!(model::JuMP.Model, data::MatpowerData, T::
         tm2 = branch["tap"]^2
         
         # Access variables for readability
-        vr_f, vi_f = model[:vr][f_bus, t], model[:vi][f_bus, t]
-        vr_t, vi_t = model[:vr][t_bus, t], model[:vi][t_bus, t]
+        c_ii = model[:c_ii][f_bus, t]
+        c_ij = model[:c_ij][(parse(Int64,f_bus), parse(Int64,t_bus)), t]
+        s_ij = model[:s_ij][(parse(Int64,f_bus), parse(Int64,t_bus)), t]
+        # p_fr = model[:p_fr][]
         p_fr, q_fr = model[:p_fr][i, t], model[:q_fr][i, t]
         p_to, q_to = model[:p_to][i, t], model[:q_to][i, t]
 
         # --- From-side power flow (Quadratic) ---
         # Real: P_fr = (g+g_fr)/tm^2 * (vr_f^2 + vi_f^2) + ...
-        @constraint(model, p_fr == (g + g_fr)/tm2 * (vr_f^2 + vi_f^2) + 
-            (-g*tr + b*ti)/tm2 * (vr_f*vr_t + vi_f*vi_t) + 
-            (-b*tr - g*ti)/tm2 * (vi_f*vr_t - vr_f*vi_t))
+        @constraint(model, p_fr == (g + g_fr)/tm2 * c_ii + 
+            (-g*tr + b*ti)/tm2 * c_ij + 
+            (-b*tr - g*ti)/tm2 * s_ij)
 
         # Reactive: Q_fr = -(b+b_fr)/tm^2 * (vr_f^2 + vi_f^2) + ...
-        @constraint(model, q_fr == -(b + b_fr)/tm2 * (vr_f^2 + vi_f^2) - 
-            (-b*tr - g*ti)/tm2 * (vr_f*vr_t + vi_f*vi_t) + 
-            (-g*tr + b*ti)/tm2 * (vi_f*vr_t - vr_f*vi_t))
+        @constraint(model, q_fr == -(b + b_fr)/tm2 * c_ii - 
+            (-b*tr - g*ti)/tm2 * c_ij + 
+            (-g*tr + b*ti)/tm2 * s_ij)
 
         # --- To-side power flow (Quadratic) ---
-        @constraint(model, p_to == (g + g_to) * (vr_t^2 + vi_t^2) + 
-            (-g*tr - b*ti)/tm2 * (vr_t*vr_f + vi_t*vi_f) + 
-            (-b*tr + g*ti)/tm2 * (vi_t*vr_f - vr_t*vi_f))
+        @constraint(model, p_to == (g + g_to) * c_ii + 
+            (-g*tr - b*ti)/tm2 * c_ij + 
+            (-b*tr + g*ti)/tm2 * s_ij)
 
-        @constraint(model, q_to == -(b + b_to) * (vr_t^2 + vi_t^2) - 
-            (-b*tr + g*ti)/tm2 * (vr_t*vr_f + vi_t*vi_f) + 
-            (-g*tr - b*ti)/tm2 * (vi_t*vr_f - vr_t*vi_f))
+        @constraint(model, q_to == -(b + b_to) * c_ii - 
+            (-b*tr + g*ti)/tm2 * c_ij + 
+            (-g*tr - b*ti)/tm2 * s_ij)
 
         # Thermal Limits (Quadratic)
         @constraint(model, p_fr^2 + q_fr^2 <= branch["rate_a"]^2)
         @constraint(model, p_to^2 + q_to^2 <= branch["rate_a"]^2)
     end
 end
-
 
 function _add_node_bal_polar!(model::JuMP.Model, data::MatpowerData, demand_curve::Vector{Float64})
 
@@ -287,151 +510,18 @@ function _add_node_bal_rectangular!(model::JuMP.Model, data::MatpowerData, deman
 
         pd = sum(l["pd"] for l in bus_loads; init=0.0)*val
         qd = sum(l["qd"] for l in bus_loads; init=0.0)*val
-        
-        local gs
-        local bs
-        # println(shunts[i])
-        if haskey(shunts, i) # if there exists gs, bs
-            println(shunts[i])
-            gs = shunts[i]["gs"]; bs = shunts[i]["bs"]
-        else
-            gs = 0; bs = 0
-        end
+
+        gs = get(bus, "gs", 0.0) + sum(get(s, "gs", 0.0) for (k,s) in shunts if string(s["shunt_bus"]) == i; init=0.0)
+        bs = get(bus, "bs", 0.0) + sum(get(s, "bs", 0.0) for (k,s) in shunts if string(s["shunt_bus"]) == i; init=0.0)
+
 
         @constraint(model, 
-            sum(model[:pg][g, t] for g in bus_gens; init=0.0) - pd - gs * (model[:vr][i, t]^2 + model[:vi][i, t]^2) == 
+            sum(model[:pg][g, t] for g in bus_gens; init=0.0) - pd - gs * model[:c_ii][i, t] == 
             sum(model[:p_fr][b, t] for b in br_fr; init=0.0) + sum(model[:p_to][b, t] for b in br_to; init=0.0)
         )
         @constraint(model, 
-            sum(model[:qg][g, t] for g in bus_gens; init=0.0) - qd + bs * (model[:vr][i, t]^2 + model[:vi][i, t]^2) == 
+            sum(model[:qg][g, t] for g in bus_gens; init=0.0) - qd + bs * model[:c_ii][i, t] == 
             sum(model[:q_fr][b, t] for b in br_fr; init=0.0) + sum(model[:q_to][b, t] for b in br_to; init=0.0)
         )
     end
 end
-
-
-function build_single_period_ac_uc_polar(file_path::String)    
-    data = _parse_file_data(file_path)
-    T = [1]
-    # Initialize the JuMP Model
-    model = Model(Gurobi.Optimizer)
-
-    # Add variables 
-    _add_acuc_var_polar!(model, data, T)
-
-    # Add objective
-    _add_mincost_obj!(model, data, T)
-
-    # Add constraints
-    _add_ref_limits_polar!(model, data, T)
-
-    _add_gen_limits!(model, data, T)
-
-    _add_polar_branchflow!(model, data, T)
-
-    _add_node_bal_polar!(model, data, T)
-
-    return model
-end
-
-
-function build_single_period_ac_uc_rectangular(file_path::String)
-    data = _parse_file_data(file_path)
-    T = [1]
-
-    # Initialize the JuMP Model
-    model = Model(Gurobi.Optimizer)
-
-    # Add variables 
-    _add_acuc_var_rectangular!(model, data, T)
-
-    # Add objective
-    _add_mincost_obj!(model, data, T)
-
-    # Add constraints
-
-    _add_ref_limits_rectangular!(model, data, T)
-    _add_gen_limits!(model, data, T)
-
-    _add_rectangular_branchflow!(model, data, T)
-
-    _add_node_bal_rectangular!(model, data, T)
-
-    return model
-end
-
-function build_mp_ac_uc_polar(file_path::String, demand_curve::Vector{Float64})
-    """
-    demand_curve::Vector{Float64} - scaled demand levels for each T
-    akin to ExaModelsPower
-    """
-    data = _parse_file_data(file_path)
-    T = [i for (i,val) in enumerate(demand_curve)]
-
-    # Initialize the JuMP Model
-    model = Model(Gurobi.Optimizer)
-
-    # Add variables 
-    _add_acuc_var_polar!(model, data, T)
-
-    # Add objective
-    _add_mincost_obj!(model, data, T)
-
-    # Add constraints
-    _add_ref_limits_polar!(model, data, T)
-
-    _add_gen_limits!(model, data, T)
-
-    _add_polar_branchflow!(model, data, T)
-
-    _add_node_bal_polar!(model, data, demand_curve)
-
-    return model
-end
-
-function build_mp_ac_uc_rectangular(file_path::String, demand_curve::Vector{Float64})
-    """
-    demand_curve::Vector{Float64} - scaled demand levels for each T
-    akin to ExaModelsPower
-    """
-    data = _parse_file_data(file_path)
-    T = [i for (i, _) in enumerate(demand_curve)]
-
-    # Initialize the JuMP Model
-    model = Model(Gurobi.Optimizer)
-
-    # Add variables 
-    _add_acuc_var_rectangular!(model, data, T)
-
-    # Add objective
-    _add_mincost_obj!(model, data, T)
-
-    # Add constraints
-
-    _add_ref_limits_rectangular!(model, data, T)
-    _add_gen_limits!(model, data, T)
-
-    _add_rectangular_branchflow!(model, data, T)
-
-    _add_node_bal_rectangular!(model, data, demand_curve)
-
-    return model
-end
-
-
-function ac_uc(file_path::String, type="Rectangular")
-    if type == "Rectangular"
-        return build_single_period_ac_uc_rectangular(file_path)
-    else
-        return build_single_period_ac_uc_polar(file_path)
-    end
-end
-
-function mp_ac_uc(file_path::String, demand_curve::Vector{Float64}, type="Rectangular")
-    if type == "Rectangular"
-        return build_mp_ac_uc_rectangular(file_path, demand_curve)
-    else
-        return build_mp_ac_uc_polar(file_path, demand_curve)
-    end
-end
-
